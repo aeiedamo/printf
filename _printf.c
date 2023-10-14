cbe68e02e292e8eb678d6ef9c340d547a4c9c3ee
MOHAMED BASHIR <138500056+sozoia@users.noreply.github.com>
2023-10-14 18:24:20 +0000

    _printf.c

---
+++ _printf.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _printf - function to print strings to stdout.
+ * @format: string
+ *Return: number of charecter printd.
+ */
+
+int _printf (const char *format, ...)
+{
+
+
+
+return ();
+}
