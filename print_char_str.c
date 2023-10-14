fd0c601bcbd51cfe065a809f3d3c1cb76f9e5ea8
AlaaEldin <alaa12122000@gmail.com>
2023-10-14 18:15:01 +0000

    Added _putc & _puts

---
+++ print_char_str.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _putc - function to print chars
+ * @c: char to print
+ * Return: 1 if success, -1 otherwise
+*/
+
+int _putc(char c)
+{
+	return(write(1, &c, 1));
+}
+
+/**
+ * _puts - function to print strings
+ * @str: pointer to string to print
+ * Return: 1 if success, -1 otherwise
+*/
+
+int _puts(const char *str)
+{
+	return(write(1, &str, 1));
+}
