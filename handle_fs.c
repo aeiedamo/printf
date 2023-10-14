ead8f9074efc6f6b3fcb96c94ba3ead7677f3f9a
sozoia <zfzg2232@gmail.com>
2023-10-14 20:37:51 +0000

    handle_fs.c

--- handle_fs.c
+++ handle_fs.c
@@ -10,9 +10,18 @@
  */
 int HANDLE_FS(char *format, int i, char buffer[], int index, va_list arg)
 {
+int i = 0;
+int fun = 'num of funcs'
+HANDLE arr []=
+{
+{'c', hand_char}, {'%', hand_per}, {'s', hand_str}
+};
 
+for (i = 0; i < fun; ++i)
+{
+	if (format[i] == arr[i].FS)
+	return (arr[i].Fs());
+}
 
-
-
-return ();
+return (-1);
 }
