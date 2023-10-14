1ee7df38370186b3bc6d06f37935cd941b89f875
sozoia <zfzg2232@gmail.com>
2023-10-14 20:31:15 +0000

    add struct to main.h

--- main.h
+++ main.h
@@ -4,6 +4,13 @@
 #include <stdlib.h>
 #include <stdarg.h>
 #include <unistd.h>
+#include <strarg.h>
+
+typedef struct str
+{
+char FS;
+int (F)(char buf[], va_list);
+}HANDLE;
 
 #define MAXBUFFER 1024
 
@@ -12,5 +19,5 @@ int _puts(const char *str);
 int _strlen(const char *str);
 int _printf(const char *format, ...);
 void print_buf(char buffer, int *index);
-
+int HANDLE_FS(char *, int, char, int, va_list);
 #endif
