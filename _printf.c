392793cad165b0a2a6bea0c25582b612987ea36d
AlaaEldin <alaa12122000@gmail.com>
2023-10-15 20:27:18 +0000

    small fix

--- _printf.c
+++ _printf.c
@@ -7,39 +7,37 @@
 */
 int _printf(const char *format, ...)
 {
-        buffer b;
-        int i;
-        va_list args;
+	buffer b;
+	int i;
+	va_list args;
 
-        b.l = 0;
-        va_start(args, format);
-        for (i = 0; format[i] != '\0'; i++)
-        {
-                if (format[i] != '%')
-                {
-                        b.d[b.l] = format[i];
-                        b.l++;
-                }
-                else
-                {
-                        i++;
-                        switch (format[i])
-                        {
-                                case 'c':
-                                        b.d[b.l] = va_arg(args, int);
-                                        b.l++;
-                                        break;
-                                case 's':
-                                        savestring(&b, args);
-                                        break;
-                                case '%':
-                                        b.d[b.l] = '%';
-                                        b.l++;
-                                        break;
-                        }
-                }
-        }
-        va_end(args);
-        write(1, &b.d, b.l);
-        return (b.l);
+	b.l = 0;
+	va_start(args, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			b.d[b.l] = format[i];
+			b.l++;
+			continue;
+		}
+		i++;
+		switch (format[i])
+		{
+			case 'c':
+				b.d[b.l] = va_arg(args, int);
+				b.l++;
+				break;
+			case 's':
+				savestring(&b, args);
+				break;
+			case '%':
+				b.d[b.l] = '%';
+				b.l++;
+				break;
+		}
+	}
+	va_end(args);
+	write(1, &b.d, b.l);
+	return (b.l);
 }
