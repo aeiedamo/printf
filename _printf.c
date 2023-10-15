afb8a7172a86794c65cb2cd8bc962b988afec999
AlaaEldin <alaa12122000@gmail.com>
2023-10-15 20:30:18 +0000

    fixes

--- _printf.c
+++ _printf.c
@@ -11,6 +11,8 @@ int _printf(const char *format, ...)
 	int i;
 	va_list args;
 
+	if (format == NULL || (format[0] == '%' && format[1] == 0))
+		return (-1);
 	b.l = 0;
 	va_start(args, format);
 	for (i = 0; format[i] != '\0'; i++)
