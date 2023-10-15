2320ad11b885e6e6e8d498e5db26a2aad7f15afd
sozoia <zfzg2232@gmail.com>
2023-10-15 12:34:00 +0000

    some problems

--- main.c
+++ main.c
@@ -9,10 +9,10 @@
  */
 int main(void)
 {
-    int len = 0;
-  char str[] = "LATTERS";
-  char c = 'R';
-    len = _printf("to day lesson : (%s) for %s : (%c) %%100 awowsme fs",str , str, c);
+  int len;
+  char c1 = 'A', c2 = 'B', c3 = 'C';
+  char *s1 = "str1", *s2 = "str2", *s3 = "str3";
+    len = _printf("this %c %c %C   %s %s %s",c1 , c2 , c3, s1, s2 ,s3);
     printf("  %d", len);
     return (0);
 }
