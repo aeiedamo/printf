125a95e70b04104e1ecc3a1c23778fb031dcb2e7
sozoia <zfzg2232@gmail.com>
2023-10-15 07:15:40 +0000

    extara changes

--- main.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-#include "main.h"
-
-int main() {
-    char c = 'R';
-    char *s = "string";
-    _printf("This is a char: (%c) and this is a string: (%s) and this is percent : (%%) ", c, s);
-    return 0;
-}
