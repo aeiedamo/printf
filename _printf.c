03ad996898cf40f93f24013acaf1a59f65570c36
AlaaEldin <alaa12122000@gmail.com>
2023-10-16 13:51:56 -0700

    Adds & Fixes

--- _printf.c
+++ _printf.c
@@ -7,28 +7,39 @@
 int _printf(const char *format, ...)
 {
 	char *empty = "", *perc = "%";
-	buffer *b = malloc(sizeof(buffer));
+	/*buffer *b = malloc(sizeof(buffer));*/
 	va_list args;
+	int i;
+	int count = 0;
 
 	if (format == NULL || format == empty || format == perc)
 		return (-1);
 
-	b->l = 0;
+	/*b->l = 0;*/
 	va_start(args, format);
-	for (; *format; format++)
+	for (i = 0; format[i] != '\0'; i++)
 	{
-		if (*format != '%')
+		if (format[i] != '%')
 		{
-			b->d[b->l++] = *format;
+			write(1, &format[i], 1);
+			count++;
+			continue;
 		}
-		else
+		i++;
+		if (format[i] == 'c')
 		{
-			format++;
-			_handle(b, args, *format);
+			char c = va_arg(args, int);
+			write(1, &c, 1);
+			count++;
 		}
+		else if (format[i] == 's')
+		{
+			char *str = va_arg(args, char *);
+
+			count += write(1, str, _strlen(str));
+		}
+		
 	}
 	va_end(args);
-	write(1, b->d, b->l);
-	free(b->d);
-	return (b->l);
+	return (count);
 }
