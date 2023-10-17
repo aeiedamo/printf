f13c2bdb46f50344df2df8aad4df14e0ffc57267
AlaaEldin <alaa12122000@gmail.com>
2023-10-17 22:10:21 +0000

    betty fixes

--- itoa.c
+++ itoa.c
@@ -22,12 +22,13 @@ void reverse(char *s)
  * itoa - convert numbers to string.
  * @val: number.
  * @base: base.
+ * Return: int as a string
 */
 char *itoa(int val, int base)
 {
 	static char buf[32] = {0};
 	int i = 30;
-	
+
 	for (; val && i; --i, val /= base)
 		buf[i] = "0123456789abcdef"[val % base];
 	return (&buf[i + 1]);
