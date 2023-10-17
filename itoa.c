46013466c739e7d7a4b93f6c080301b864d4d37d
AlaaEldin <alaa12122000@gmail.com>
2023-10-17 00:51:48 -0700

    Adds & Fixes

--- itoa.c
+++ itoa.c
@@ -10,17 +10,12 @@
 char *itoa(int val, int base)
 {
 	static char buf[32] = {0};
-	int i = 31;
+	int i = 30;
 
 	if (val < 0)
 		val *= -1;
 	for (; val && i ; --i, val /= base)
 		buf[i] = "0123456789abcdef"[val % base];
-	
-	if (val < 0)
-		buf[i] = '-';
-
 	return (&buf[i + 1]);
-
 }
 
