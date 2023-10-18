7ca078ed855821595edc46c9e14b63ac18f2a74d
AlaaEldin <alaa12122000@gmail.com>
2023-10-18 00:24:39 +0000

    last push

--- printpointer.c
+++ printpointer.c
@@ -8,18 +8,18 @@
 
 int printpointer(va_list args)
 {
-        void *ptr;
-        unsigned long int num;
-        int count;
+	void *ptr;
+	unsigned long int num;
+	int count;
 
-        ptr = va_arg(args, void *);
-        if (ptr == NULL)
-        {
-                _printf("(nil)");
-                return (_strlen("(nil)"));
-        }
+	ptr = va_arg(args, void *);
+	if (ptr == NULL)
+	{
+		_printf("(nil)");
+		return (_strlen("(nil)"));
+	}
 
-        num = (unsigned long int) ptr;
-        count = _printf("0x%x", num);
-        return (count);
+	num = (unsigned long int) ptr;
+	count = _printf("0x%x", num);
+	return (count);
 }
